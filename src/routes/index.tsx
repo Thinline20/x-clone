40687d64736df50ff08d5e7c4ab4cc07e0adf826
Thinline20 <thinline20@gmail.com>
2023-11-13 14:27:47 +0900

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main class="container mx-auto">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin capitalize my-16">
+        Hello world!
+      </h1>
+    </main>
+  );
+}
